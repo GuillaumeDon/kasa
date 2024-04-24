712f63001f3d776238608e9638cba544b1b440a0
Guillaume Donizetti <glm.donizetti@gmail.com>
2024-04-24 19:17:15 +0200

    training

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,3 +1,6 @@
+import '../styles/Banner.css'
+import logo from '../assets/logo.png'
+
 function Banner() {
     return (
       <div className="Banner">
@@ -6,6 +9,7 @@ function Banner() {
           <h1>
             Début du projet Kasa
           </h1>
+          <img src={logo} alt="logo du site" className='logo-site'></img>
   
         </header>
       </div>
