9713af2ddca6cd718c4b416b6b377827ac652c75
Guillaume Donizetti <glm.donizetti@gmail.com>
2024-05-02 21:56:30 +0200

    maj

--- src/components/Host/index.jsx
+++ src/components/Host/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Host({ name, picture }) {
   return (
-    <div>
+    <div className='host-container'>
       <h3>{name}</h3>
       <img src={picture} alt={name} />
     </div>
