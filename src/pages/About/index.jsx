5a427d67a9f5afc410a6a7d5a9246aaa4b2c81a0
Guillaume Donizetti <glm.donizetti@gmail.com>
2024-05-21 20:17:04 +0200

    maj about

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,28 +1,34 @@
 import React from 'react';
 import Collapse from '../../components/Collapse';
 import Banner from '../../components/Banner'; 
-import CoverPictureAbout from '../../assets/picture-about.png'
-
+import CoverPictureAbout from '../../assets/picture-about.png';
 
 function About() {
-
     const title = "";
     return (
         <div className='about-container'>
             <Banner title={title} imageUrl={CoverPictureAbout}  />
             
-            <Collapse title="Fiabiité">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem accusamus perspiciatis, ex neque hic quam dolor itaque voluptatum laborum cum similique optio accusantium temporibus, aliquid necessitatibus. Aliquid at temporibus quam?
-            </Collapse>
-            <Collapse title="Respect">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem accusamus perspiciatis, ex neque hic quam dolor itaque voluptatum laborum cum similique optio accusantium temporibus, aliquid necessitatibus. Aliquid at temporibus quam?
-            </Collapse>
-            <Collapse title="Service">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem accusamus perspiciatis, ex neque hic quam dolor itaque voluptatum laborum cum similique optio accusantium temporibus, aliquid necessitatibus. Aliquid at temporibus quam?
-            </Collapse>
-            <Collapse title="Sécurité">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem accusamus perspiciatis, ex neque hic quam dolor itaque voluptatum laborum cum similique optio accusantium temporibus, aliquid necessitatibus. Aliquid at temporibus quam?
-            </Collapse>
+            <div className='collapse-container'>
+                <Collapse title="Fiabiité">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem accusamus perspiciatis, ex neque hic quam dolor itaque voluptatum laborum cum similique optio accusantium temporibus, aliquid necessitatibus. Aliquid at temporibus quam?
+                </Collapse>
+            </div>
+            <div className='collapse-container'>
+                <Collapse title="Respect">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem accusamus perspiciatis, ex neque hic quam dolor itaque voluptatum laborum cum similique optio accusantium temporibus, aliquid necessitatibus. Aliquid at temporibus quam?
+                </Collapse>
+            </div>
+            <div className='collapse-container'>
+                <Collapse title="Service">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem accusamus perspiciatis, ex neque hic quam dolor itaque voluptatum laborum cum similique optio accusantium temporibus, aliquid necessitatibus. Aliquid at temporibus quam?
+                </Collapse>
+            </div>
+            <div className='collapse-container'>
+                <Collapse title="Sécurité">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem accusamus perspiciatis, ex neque hic quam dolor itaque voluptatum laborum cum similique optio accusantium temporibus, aliquid necessitatibus. Aliquid at temporibus quam?
+                </Collapse>
+            </div>
         </div>
     )
 }
--- src/pages/Error404/index.jsx
+++ src/pages/Error404/index.jsx
@@ -3,7 +3,7 @@ function Error404() {
     return (
         <div className='error-container'>
             <h1 className='error-container-title'>404</h1>
-            <p className='error-container-txt'>Oups! a page que vous demandez n'existe pas !</p>
+            <p className='error-container-txt'>Oups! La page que<br className="mobile-only"/> vous demandez n'existe pas !</p>
             <Link className='error-container-link' to="/">Retourner sur la page d'accueil</Link>
         </div>
     )
