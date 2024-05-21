b96e64c5fa0b92ade2bb838f4977c2541706071a
Guillaume Donizetti <glm.donizetti@gmail.com>
2024-05-21 20:34:55 +0200

    maj produits

--- src/pages/ProductDetail/index.jsx
+++ src/pages/ProductDetail/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { useParams, Navigate } from 'react-router-dom';
 import jsonData from './../../data/data.json'; 
 import Collapse from '../../components/Collapse';
@@ -21,34 +20,34 @@ function ProductDetail() {
     return <Navigate to="/404" />;
   }
 
-  // Affichez les détails du produit
+
   return (
     <div className='product-container'>
-      
       <Slideshow images={product.pictures}/>
       {/* <img src={product.cover} alt={product.title} /> */}
       <h2 className='product-container-title'>{product.title}</h2>
       <h3 className='product-container-location'>{product.location}</h3>
       <Tags productId={id}/>
       <div className='product-container-infos'>
-      <Rating rating={product.rating} />
-      <Host name={product.host.name} picture={product.host.picture} />
+        <Rating rating={product.rating} />
+        <Host name={product.host.name} picture={product.host.picture} />
       </div>
       <div className='product-container-collapse'>
-      <Collapse title="Description">
-      <p>{product.description}</p>
-      </Collapse>
-      
-      <Collapse title="Equipements">
-
-      <ul>
-        {product.equipments.map((equipment, index) => (
-          <li key={index}>{equipment}</li>
-        ))}
-      </ul>
-      </Collapse>
-      
-    </div>
+        <div className='collapse-container'>
+          <Collapse title="Description">
+            <p>{product.description}</p>
+          </Collapse>
+        </div>
+        <div className='collapse-container'>
+          <Collapse title="Equipements">
+            <ul>
+              {product.equipments.map((equipment, index) => (
+                <li key={index}>{equipment}</li>
+              ))}
+            </ul>
+          </Collapse>
+        </div>
+      </div>
     </div>
   );
 }
