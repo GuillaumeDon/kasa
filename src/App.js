89272095cebc5b49b35270e1425299926a795cfe
Guillaume Donizetti <glm.donizetti@gmail.com>
2024-06-09 01:14:43 +0200

    fix du footer

--- src/App.js
+++ src/App.js
@@ -1,3 +1,37 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Home from './pages/Home/';
+// import About from './pages/About/';
+// import Error404 from './pages/Error404';
+// import ProductDetail from './pages/ProductDetail';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+// import './styles/main.css';
+
+
+// function App() {
+//   return (
+//     <div className="app-container">
+//       <Router>
+//         <Header />
+//         <main className="main-content">
+//           <Routes>
+//             <Route path="/product/:id" element={<ProductDetail />} />
+//             <Route path="/about" element={<About />} />
+//             <Route path="/" element={<Home />} />
+//             <Route path="/404/" element={<Error404 />} />
+//             <Route path="*" element={<Error404 />} />
+//           </Routes>
+//         </main>
+//         <Footer />
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/';
@@ -7,7 +41,6 @@ import ProductDetail from './pages/ProductDetail';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './styles/main.css';
-
 
 function App() {
   return (
