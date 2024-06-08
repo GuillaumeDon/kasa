ec7cab948c5dae224114483571ddd63273d64e20
Guillaume Donizetti <glm.donizetti@gmail.com>
2024-06-09 01:37:59 +0200

    maj name en colonne

--- src/components/Host/index.jsx
+++ src/components/Host/index.jsx
@@ -1,9 +1,13 @@
+// index.jsx
+
 import React from 'react';
 
 function Host({ name, picture }) {
+  const formattedName = name.split(' ').join('\n');
+
   return (
     <div className='host-container'>
-      <h3 className='host-container-name'>{name}</h3>
+      <h3 className='host-container-name'>{formattedName}</h3>
       <img className='host-container-img' src={picture} alt={name} />
     </div>
   );
