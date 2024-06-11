1bf3932d93438e6758057119385a212a8cea5c1e
Guillaume Donizetti <glm.donizetti@gmail.com>
2024-06-11 16:23:46 +0200

    nettoyage

--- src/components/Arrows/index.jsx
+++ src/components/Arrows/index.jsx
@@ -1,30 +1,3 @@
-// import React from 'react';
-// import logoArrow from '../assets/arrow.png'
-
-// export function ArrowUpIcon() {
-//   return (
-//     <img src={logoArrow} alt="Arrow Up" className="arrow-icon" />
-//   );
-// }
-
-// export function ArrowDownIcon() {
-//   return (
-//     <img src={logoArrow} alt="Arrow Down" className="arrow-icon" style={{ transform: 'rotate(180deg)' }} />
-//   );
-// }
-
-// export function ArrowleftIcon() {
-//   return (
-//     <img src={logoArrow} alt="Arrow Down" className="arrow-icon" style={{ transform: 'rotate(90deg)' }} />
-//   );
-// }
-
-// export function ArrowRightIcon() {
-//   return (
-//     <img src={logoArrow} alt="Arrow Down" className="arrow-icon" style={{ transform: 'rotate(270deg)' }} />
-//   );
-// }
-
 
 import React from 'react';
 import logoArrow from '../../assets/arrow.png';
